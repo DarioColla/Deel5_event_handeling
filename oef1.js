a6c932b4a2938c5ec26e745ae02f5762b9950d9d
DarioColla <12202253@student.pxl.be>
2024-03-18 18:04:14 +0100

    laatste versie

--- oef1.js
+++ oef1.js
@@ -1,24 +1,21 @@
 let clickTeller = 0;
 
 document.getElementById("knopje").addEventListener("click", toonTekst);
+function toonTekst(){
 
-function toonTekst() {
     let stukjesTekst = document.getElementById("tekstveld").value;
     let tekstParagraaf = document.createTextNode(stukjesTekst);
-    let bestaandParagraaf = document.getElementById("tekstgebruiker");
 
-    if (clickTeller === 0) {
+    if (clickTeller ===0){
         clickTeller++;
-        bestaandParagraaf.appendChild(tekstParagraaf);
-    } else {
-        // Verwijder het bestaande paragrafelement
-        bestaandParagraaf.parentNode.removeChild(bestaandParagraaf);
-
-        // Creëer een nieuw paragrafelement en voeg het toe aan de body
-        let nieuwParagraaf = document.createElement("p");
-        nieuwParagraaf.id = "tekstgebruiker";
-        nieuwParagraaf.appendChild(tekstParagraaf);
-        document.body.appendChild(nieuwParagraaf);
+        document.getElementById("tekstgebruiker").appendChild(tekstParagraaf);
     }
+
+    else{
+        document.getElementById("tekstgebruiker").removeChild(tekstParagraaf);
+        document.getElementById("tekstgebruiker").appendChild(tekstParagraaf);
+
+    }
+
 }
 
